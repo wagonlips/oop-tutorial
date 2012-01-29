7175be623844ba278bbae84a2ce5bac9a7129048
Sean Camden <sean@seancamden.com>
2012-01-29 14:36:22 -0800

    Added a __constructor to the person class.

--- class_lib.php
+++ class_lib.php
@@ -1,6 +1,9 @@
 <?php  
 class person { 
   var $name;  
+  function __construct($persons_name) { 
+    $this->name = $persons_name; 
+  } 
   function set_name($new_name) {  
     $this->name = $new_name;   
   }      
--- index.php
+++ index.php
@@ -8,10 +8,8 @@
 </head> 
 <body> 
 <?php 
-  $stefan = new person(); 
-  $jimmy = new person; 
-  $stefan->set_name("Stefan Mischook"); 
-  $jimmy->set_name("Nick Waddles");
+  $stefan = new person("Stefan Mischook"); 
+  $jimmy = new person("Nick Waddles"); 
   echo "Stefan's full name: " . $stefan->get_name() . "<br />"; 
   echo "Nick's full name: " . $jimmy->get_name() . "<br />";
 ?>
