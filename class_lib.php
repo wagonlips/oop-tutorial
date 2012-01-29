f8a6b00b44d57cbc5dfe39f1a39310c89457a45b
Sean Camden <sean@seancamden.com>
2012-01-29 14:21:37 -0800

    Added person class. Added .gitignore ignoring .swp files.

---
+++ class_lib.php
@@ -0,0 +1,11 @@
+<?php  
+class person { 
+  var $name;  
+  function set_name($new_name) {  
+    $this->name = $new_name;   
+  }      
+  function get_name() { 
+    return $this->name;  
+  }  
+}  
+?>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
+<html xmlns="http://www.w3.org/1999/xhtml"> 
+<head> 
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" /> 
+<title>OOP in PHP</title> 
+<?php include("class_lib.php"); ?> 
+</head> 
+<body> 
+<?php 
+  $stefan = new person(); 
+  $jimmy = new person; 
+  $stefan->set_name("Stefan Mischook"); 
+  $jimmy->set_name("Nick Waddles");
+  echo "Stefan's full name: " . $stefan->get_name() . "<br />"; 
+  echo "Nick's full name: " . $jimmy->get_name() . "<br />";
+?>
+</body> 
+</html
